894eb9464fe55f38f1b76c4b178fced63712b85f
Raj106292 <rajdas.106292@gmail.com>
2022-11-24 01:14:24 +0600

    auth provider implemented

--- src/Contexts/AuthProvider.js
+++ src/Contexts/AuthProvider.js
@@ -1,11 +1,96 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react'
+import { createContext } from 'react'
+import {
+    createUserWithEmailAndPassword,
+    getAuth,
+    GoogleAuthProvider,
+    onAuthStateChanged,
+    sendPasswordResetEmail,
+    signInWithEmailAndPassword,
+    signInWithPopup,
+    signOut,
+    updateProfile,
+} from 'firebase/auth'
+import app from '../firebase/firebase.config'
 
-const AuthProvider = () => {
+export const AuthContext = createContext()
+const auth = getAuth(app)
+
+const googleProvider = new GoogleAuthProvider()
+
+const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null)
+    const [loading, setLoading] = useState(true)
+    console.log(user)
+
+    // create new user
+    const createUser = (email, password) => {
+        setLoading(true)
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+    //   update user name and photo
+    const updateUserProfile = (name, photo) => {
+        setLoading(true)
+        return updateProfile(auth.currentUser, {
+            displayName: name,
+            photoURL: photo,
+        })
+    }
+
+    // google sign in
+    const signInWithGoogle = () => {
+        setLoading(true)
+        return signInWithPopup(auth, googleProvider)
+    }
+
+    // sign out
+    const logout = () => {
+        setLoading(true)
+        // localStorage.removeItem('aircnc-token')
+        return signOut(auth)
+    }
+
+    // user login
+    const login = (email, password) => {
+        setLoading(true)
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+
+    //7. Forget Password
+    const resetPassword = email => {
+        setLoading(true)
+        return sendPasswordResetEmail(auth, email)
+    }
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser)
+            setLoading(false)
+        })
+
+        return () => {
+            unsubscribe()
+        }
+    }, [])
+
+    const authInfo = {
+        user,
+        createUser,
+        updateUserProfile,
+        signInWithGoogle,
+        logout,
+        login,
+        resetPassword,
+        loading,
+        setLoading,
+    }
+
     return (
-        <div>
-            
-        </div>
-    );
-};
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
 
-export default AuthProvider;
+export default AuthProvider
