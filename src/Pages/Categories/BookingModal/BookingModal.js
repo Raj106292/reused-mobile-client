04154528f91fbb2ef50bdbf1889bec9ffd8a9de6
Raj106292 <rajdas.106292@gmail.com>
2022-11-26 03:08:18 +0600

    booking modal done

---
+++ src/Pages/Categories/BookingModal/BookingModal.js
@@ -0,0 +1,141 @@
+import React, { useContext, useState } from 'react';
+import toast from 'react-hot-toast';
+import Loader from '../../../Common/Loader';
+import { AuthContext } from '../../../Contexts/AuthProvider';
+
+const BookingModal = ({ booking, setBooking }) => {
+
+    const { user } = useContext(AuthContext);
+    const [bmLoading, setBmLoading] = useState(false);
+
+    const handleBooking = e => {
+        e.preventDefault();
+        setBmLoading(true);
+
+        const form = e.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        const product = form.product.value;
+        const phone = form.phone.value;
+        const location = form.location.value;
+        const image = booking.image;
+
+        const bookingData = {
+            name,
+            email,
+            product,
+            phone,
+            location,
+            image,
+        }
+
+        fetch(`${process.env.REACT_APP_server}/bookings`, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+            },
+            body: JSON.stringify(bookingData)
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.acknowledged) {
+                    toast.success('your booking is confirmed', { duration: 3000 });
+                    setBmLoading(false);
+                    setBooking(null);
+                }
+            })
+            .catch(error => {
+                toast.error(error.message, { duration: 3000 })
+                setBmLoading('false');
+            })
+    }
+
+    return (
+        <div>
+            <input type="checkbox" id="booking-modal" className="modal-toggle" />
+            <div className="modal">
+                <div className="modal-box relative">
+                    <label htmlFor="booking-modal" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+                    <h3 className="text-lg font-bold text-center text-blue-600">Booking Form</h3>
+                    <form className='mt-5 space-y-3' onSubmit={handleBooking}>
+                        <div className="form-control w-full">
+                            <input
+                                type="text"
+                                name='name'
+                                placeholder="your name"
+                                defaultValue={user?.displayName}
+                                disabled
+                                className="input input-bordered w-full"
+                            />
+                        </div>
+                        <div className="form-control w-full">
+                            <input
+                                type="email"
+                                name='email'
+                                placeholder="your email"
+                                defaultValue={user?.email}
+                                disabled
+                                className="input input-bordered w-full"
+                            />
+                        </div>
+                        <div className="form-control w-full">
+                            <input
+                                type="text"
+                                name='product'
+                                placeholder="product"
+                                defaultValue={booking?.model}
+                                disabled
+                                className="input input-bordered w-full"
+                            />
+                        </div>
+                        <div className="form-control w-full">
+                            <input
+                                type="text"
+                                name='price'
+                                placeholder="price"
+                                defaultValue={booking?.resalePrice}
+                                disabled
+                                className="input input-bordered w-full"
+                            />
+                        </div>
+                        <div className="form-control w-full">
+                            <label className="label">
+                                <span className="label-text">Phone:</span>
+                            </label>
+                            <input
+                                type="text"
+                                name='phone'
+                                placeholder="phone number"
+                                required
+                                className="input input-bordered w-full"
+                            />
+                        </div>
+                        <div className="form-control w-full">
+                            <label className="label">
+                                <span className="label-text">Meeting Place:</span>
+                            </label>
+                            <input
+                                type="text"
+                                name='location'
+                                placeholder="where you want to meet?"
+                                required
+                                className="input input-bordered w-full"
+                            />
+                        </div>
+                        <button className="btn btn-info btn-outline w-full">
+                            {
+                                bmLoading ? <Loader /> :
+                                    <input
+                                        type="submit"
+                                        required
+                                    />
+                            }
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default BookingModal;
---
+++ src/Pages/Categories/Product/Product.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+const Product = ({prd, setBooking}) => {
+    return (
+        <div>
+            <div className="card w-96 bg-base-100 shadow-xl">
+                <figure><img src={prd?.image} className='w-full' style={{ height: '250px' }} alt="Shoes" /></figure>
+                <div className="card-body">
+                    <h2 className="card-title">{prd?.model}</h2>
+                    <p>{prd?.description}</p>
+                    <div className='flex justify-start items-center'>
+                        <p className='font-bold'>Seller:
+                            <span className='text-blue-500'> {prd?.sellerName}</span>
+                        </p>
+                        <p className='ml-[-160px]'>
+                            {
+                                prd?.sellerStatus === 'verified' &&
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="blue" viewBox="0 0 24 24" strokeWidth={1.5} stroke="white" className="w-6 h-6">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 01-1.043 3.296 3.745 3.745 0 01-3.296 1.043A3.745 3.745 0 0112 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 01-3.296-1.043 3.745 3.745 0 01-1.043-3.296A3.745 3.745 0 013 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 011.043-3.296 3.746 3.746 0 013.296-1.043A3.746 3.746 0 0112 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 013.296 1.043 3.746 3.746 0 011.043 3.296A3.745 3.745 0 0121 12z" />
+                                </svg>
+                            }
+                        </p>
+                    </div>
+                    <p className='font-bold'>Location: <span className='text-blue-500'>{prd?.sellerLocation}</span></p>
+                    <p className='font-bold'>Years of use: <span className='text-blue-500'>{prd?.yearsOfUse}</span></p>
+                    <p className='font-bold'>Date of Post: <span className='text-blue-500'>{prd?.date}</span></p>
+                    <p className='font-bold'>Resale Price: BDT- <span className='text-blue-500'>{prd?.resalePrice}</span></p>
+                    <p className='font-bold'>Original Price: BDT- <span className='text-blue-500'>{prd?.originalPrice}</span></p>
+                    <div className="card-actions justify-between items-center">
+                        <button className='btn btn-outline btn-warning'>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="red" viewBox="0 0 24 24" strokeWidth={1.5} stroke="red" className="w-6 h-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+                            </svg>
+                        </button>
+                        <label 
+                        htmlFor="booking-modal" 
+                        onClick={() => setBooking(prd)}
+                        className="btn btn-primary"
+                        >Book Now</label>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Product;
